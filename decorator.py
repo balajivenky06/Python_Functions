e27ad0963b341b3031df618f1f806ca68a0972ec
Balaji Venktesh <balajivenky06@gmail.com>
2021-11-15 18:15:55 +0530

    adding decorator.py that explains decorators in python using statements.py file

---
+++ decorator.py
@@ -0,0 +1,21 @@
+#learning Decorators
+
+from functools import wraps
+from time import time
+from statements import lazy_return_random_attacks #import function from statements.py file
+
+def timing(f):
+    @wraps(f)
+    def wrap(*args, **kw):
+        ts = time()
+        result = f(*args, **kw)
+        te = time()
+        print(f"fun: {f.__name__}, args: [{args}, {kw}] took: {te-ts} sec")
+        return result
+    return wrap
+
+@timing
+def some_attachs():
+    attack = lazy_return_random_attacks()
+    for _ in range(5): 
+        print(next(attack)) #notice this function is passed into decorator and calculates total time taken
