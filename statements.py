41d70a2ec886d0baa2dcadcd9dbff3e5e8cbc7d3
Balaji Venktesh <balajivenky06@gmail.com>
2021-11-15 17:56:57 +0530

    statements.py file that contains lazy generators example

---
+++ statements.py
@@ -0,0 +1,20 @@
+#Generator Example
+def lazy_return_random_attacks():
+    """Yield attacks each time"""
+    import random
+    attacks = {"kimura": "upper_body",
+               "straight_ankle_lock": "lower_body",
+               "arm_triangle": "upper_body",
+               "keylock": "upper_body",
+               "knee_bar": "lower_body"}
+    
+    while True:
+        random_attack = random.choices(list(attacks.key))
+        yield random_attack
+
+attack = lazy_return_random_attacks()
+print(type(attack))
+
+for _ in range(3):
+    print(next(attack))
+
